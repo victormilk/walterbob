cb1adf640e43e71ef2043d86c8aa47caa577faa8
Victor Leite Costa <victor.milkcosta@gmail.com>
2023-08-16 01:24:13 -0300

    add work module

---
+++ src/main/kotlin/io/github/victormilk/walterbob/modules/work/application/web/WorkController.kt
@@ -0,0 +1,15 @@
+package io.github.victormilk.walterbob.modules.work.application.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/works")
+class WorkController {
+
+    @GetMapping
+    suspend fun getWorks(): String {
+        return "dev -> works"
+    }
+}
